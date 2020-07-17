a80dd6ab4c03b04714650b7888481f43499248b5
Ebubekir Sezer <ebubekirssezer@gmail.com>
2020-07-17 15:07:54 +0300

    Post Model added

--- app/src/main/java/com/ess/kotlinretrofitsample/view/MainActivity.kt
+++ app/src/main/java/com/ess/kotlinretrofitsample/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ess.kotlinretrofitsample
+package com.ess.kotlinretrofitsample.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ess.kotlinretrofitsample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
